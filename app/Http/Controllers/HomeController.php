13e2451e52274b1022a5e30b6a641013b1a262af
mahmoued <mahmouedmartin222@yahoo.com>
2020-06-11 22:36:21 +0200

    Added posts to homepage.

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -1,8 +1,8 @@
 <?php
 
 namespace App\Http\Controllers;
-
-use Illuminate\Http\Request;
+use App\Http\Controllers\PostsController;
+use App\Post;
 
 class HomeController extends Controller
 {
@@ -23,6 +23,8 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('home');
+        return view('home',[
+            'posts' => Post::latest()->get()
+        ]);
     }
 }
